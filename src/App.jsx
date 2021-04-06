c48c9205e030d5ecf0eeb492fab900633321b268
Chris Winkler <procy.dev@gmail.com>
2021-04-06 13:13:41 -0700

    Added styles using useStyles hook

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,22 @@ import React from 'react';
 import { Typography, AppBar, Button, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@material-ui/core';
 import { PhotoCamera } from '@material-ui/icons';
 
+import useStyles from './styles'
+
 const App = () => {
+    const classes = useStyles();
+
     return (
         <>
             <CssBaseline />
             <AppBar position="relative">
                 <Toolbar>
-                    <PhotoCamera />
+                    <PhotoCamera className={classes.icon}/>
                     <Typography variant="h6">Photo Album</Typography>
                 </Toolbar>
             </AppBar>
             <main>
-                <div>
+                <div className={classes.container}>
                     <Container maxWidth="sm">
                         <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
                             Photo Album
@@ -21,7 +25,7 @@ const App = () => {
                         <Typography variant="h5" align="center" color="textSecondary" paragraph>
                             This is a long sentence to see how the format of the typography under the container looks. Doing stuff and things to practice material-ui and react stuff!
                         </Typography>
-                        <div>
+                        <div className={classes.buttons}>
                             <Grid container spacing={2} justify="center">
                                 <Grid item>
                                     <Button variant="contained" color="primary">
