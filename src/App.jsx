ef64b96e562a833e8dac9b84b09463e8d31a9021
Chris Winkler <procy.dev@gmail.com>
2021-04-06 13:25:16 -0700

    Added page responsiveness for cards

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { PhotoCamera } from '@material-ui/icons';
 
 import useStyles from './styles'
 
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
 const App = () => {
     const classes = useStyles();
 
@@ -43,7 +45,8 @@ const App = () => {
                 </div>
                 <Container className={classes.cardGrid} maxWidth="md">
                     <Grid container spacing={4}>
-                        <Grid item>
+                    {cards.map((card) => (
+                        <Grid item key={card} sx={12} sm={6} md={4}>
                             <Card className={classes.card}>
                                 <CardMedia 
                                     className={classes.cardMedia}
@@ -64,6 +67,7 @@ const App = () => {
                                 </CardActions>
                             </Card>
                         </Grid>
+                    ))}                       
                     </Grid>
                 </Container>
             </main>
