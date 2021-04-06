418d0f925def33bf21f009dc228418a59e5f7c79
Chris Winkler <procy.dev@gmail.com>
2021-04-06 13:20:49 -0700

    Added a card to the page with media/description/buttons as well as basic styling for it

--- src/App.jsx
+++ src/App.jsx
@@ -41,6 +41,31 @@ const App = () => {
                         </div>
                     </Container>
                 </div>
+                <Container className={classes.cardGrid} maxWidth="md">
+                    <Grid container spacing={4}>
+                        <Grid item>
+                            <Card className={classes.card}>
+                                <CardMedia 
+                                    className={classes.cardMedia}
+                                    image="https://source.unsplash.com/random"
+                                    title="Image title"
+                                />
+                                <CardContent className={classes.cardContent}>
+                                    <Typography gutterBottom variant="h5">
+                                        Heading
+                                    </Typography>
+                                    <Typography>
+                                        This is a media card. You can use this section to describe the content of the card.
+                                    </Typography>
+                                </CardContent>
+                                <CardActions>
+                                    <Button size="small" color="primary">View</Button>
+                                    <Button size="small" color="secondary">Edit</Button>
+                                </CardActions>
+                            </Card>
+                        </Grid>
+                    </Grid>
+                </Container>
             </main>
         </>
     );
